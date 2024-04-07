2e51eaa558cbff0eb9c0392f2be27c2d43821213
Anurag Jaisingh <anukhg@gmail.com>
2024-04-08 03:05:39 +0530

    git alias success message

--- development-config-setup/git-alias-setup.sh
+++ development-config-setup/git-alias-setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Show a summarized git log
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 
@@ -82,3 +84,10 @@ git config --global alias.checkremote "!f() { git fetch origin && git branch --r
 # Check if local branch is present or not
 git config --global alias.checklocal "!f() { git branch --list | grep -q \"$1\" && echo \"Branch '$1' exists locally\" || echo \"Branch '$1' does not exist locally\"; }; f"
 
+
+# Check if all commands were successful and display a message
+if [ $? -eq 0 ]; then
+    echo "All Git aliases added successfully."
+else
+    echo "Error occurred while adding Git aliases."
+fi
